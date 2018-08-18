cd8d151b6472bb153f3621f63a1e54490104c82f
Kayl Eubanks <kayleubanks@gmail.com>
2018-08-17 17:39:56 -0700

    finish multiple of 4 challenge

--- beginner_exercises/odd_even.py
+++ beginner_exercises/odd_even.py
@@ -3,7 +3,9 @@ def odd_even():
     input_prompt = 'Please enter a number: '
     number = int(input(input_prompt))
     r = number % 2
-    if r is 0:
+    if r is 0 and (number%4) is 0:
+        print('this number is divisible by 4!')
+    elif r is 0:
         print(str(number) + ' is an even number.')
     else:
         print(str(number) + ' is an odd number.')
