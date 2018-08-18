8abce13d8de7c7750dcc77c92ec4bd425e5e0ac5
Kayl Eubanks <kayleubanks@gmail.com>
2018-08-17 17:37:23 -0700

    finish odd_even basic challenge

--- beginner_exercises/odd_even.py
+++ beginner_exercises/odd_even.py
@@ -1,3 +1,9 @@
 def odd_even():
     """Take number, print out appropriate message to user whether number is even or odd"""
-    
+    input_prompt = 'Please enter a number: '
+    number = int(input(input_prompt))
+    r = number % 2
+    if r is 0:
+        print(str(number) + ' is an even number.')
+    else:
+        print(str(number) + ' is an odd number.')
