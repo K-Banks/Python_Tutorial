f4cb36ab156d48f87c06f6c1a36df66741617786
Kayl Eubanks <kayleubanks@gmail.com>
2018-08-17 17:28:52 -0700

    Complete additional challenges for exercise

--- beginner_exercises/character_input.py
+++ beginner_exercises/character_input.py
@@ -7,4 +7,7 @@ def name_age():
     print(age_prompt)
     age = int(input(""))
     one_hundred = str((100 - age) + this_year)
-    print('Hello ' + name + '. You will be 100 years old in the year ' + one_hundred + '.')
+    print('How many times do you want to hear this?')
+    repeat = int(input(""))
+    for numbers in range(repeat):
+        print('Hello ' + name + '. You will be 100 years old in the year ' + one_hundred + '.\n')
